285746468e9a83a56612b6a95d5c098fb876839a
jiyloniy <qodirxonyusufjanov5@gmail.com>
2026-04-18 21:49:42 +0500

    Update ALLOWED_HOSTS to allow all hosts for development

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['62.171.134.170', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
