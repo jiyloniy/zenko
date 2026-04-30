4a1c34bbad1ad90c25d8743e773f6e0f993c65a2
jiyloniy <qodirxonyusufjanov5@gmail.com>
2026-04-30 07:25:12 +0500

    refactor: Remove QabulJarayon admin class and its related imports

--- apps/tosh/admin.py
+++ apps/tosh/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tosh, ToshQadashJarayon, ToshQadashLog, KleyRasxod, ToshRasxod, QabulJarayon
+from .models import Tosh, ToshQadashJarayon, ToshQadashLog, KleyRasxod, ToshRasxod
 
 
 @admin.register(Tosh)
@@ -90,18 +90,3 @@ class ToshRasxodAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
-@admin.register(QabulJarayon)
-class QabulJarayonAdmin(admin.ModelAdmin):
-    list_display  = ('tosh_jarayon', 'status', 'updated_by', 'updated_at')
-    list_filter   = ('status', 'created_at', 'updated_at')
-    search_fields = ('tosh_jarayon__order__name',)
-    readonly_fields = ('created_at', 'updated_at')
-    fieldsets = (
-        ('Asosiy', {'fields': ('tosh_jarayon', 'status')}),
-        ('Eslatma', {'fields': ('izoh',)}),
-        ('Meta', {'fields': ('updated_by', 'created_at', 'updated_at'), 'classes': ('collapse',)}),
-    )
-
-    def save_model(self, request, obj, form, change):
-        obj.updated_by = request.user
-        super().save_model(request, obj, form, change)
